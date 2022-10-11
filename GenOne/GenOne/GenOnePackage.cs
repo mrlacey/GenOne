2cc0309439b19c34dd9e55369cf837dc08b9883c
Matt Lacey <matt@mrlacey.co.uk>
2022-10-11 14:11:49 +0100

    initial SFG infrastructure

---
+++ GenOne/GenOne/GenOnePackage.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Community.VisualStudio.Toolkit;
+using GenOne.Generators;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace GenOne
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.GenOneCsGeneratorString)]
+
+    [ProvideCodeGenerator(typeof(GenOneCsharpGenerator), GenOneCsharpGenerator.Name, GenOneCsharpGenerator.Description, true, RegisterCodeBase = true)]
+    [ProvideCodeGeneratorExtension(GenOneCsharpGenerator.Name, ".gen1")]
+    [ProvideUIContextRule(PackageGuids.CommandVisisiblityString,
+        name: "XAML files",
+        expression: "IsXaml",
+        termNames: new[] { "IsXaml" },
+        termValues: new[] { "HierSingleSelectionName:.xaml$" })]
+    internal class GenOnePackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
+}
---
+++ GenOne/GenOne/Generators/GenOneCsharpGenerator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TextTemplating.VSHost;
+
+namespace GenOne.Generators
+{
+    // TODO: also add a code snippet for 'ltb'
+    // TODO: Also add item template for a .gen1 file
+    internal class GenOneCsharpGenerator : BaseCodeGeneratorWithSite
+    {
+        public const string Name = nameof(GenOneCsharpGenerator);
+
+        public const string Description = "Create C# files from GenOne docs";
+
+        public override string GetDefaultExtension() => ".cs";
+
+        protected override byte[] GenerateCode(string inputFileName, string inputFileContent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
